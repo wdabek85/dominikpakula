075e641f5b7c56bb2812d91b31dfb2bcbe133df3
wdabek85 <wdabek85@gmail.com>
2026-07-07 18:42:22 +0200

    Filozofia: usuniete zbedne pola ACF (avatar, label) - zostaje cytat + zdjecie

--- public/app/themes/dominikpakula/app/View/Composers/ManifestBlockComposer.php
+++ public/app/themes/dominikpakula/app/View/Composers/ManifestBlockComposer.php
@@ -12,14 +12,10 @@ class ManifestBlockComposer extends Composer
 
     public function with(): array
     {
-        $avatar = \get_field('manifest_avatar');
         $image = \get_field('manifest_image');
 
         return [
             'text' => \get_field('manifest_text') ?: '',
-            'label' => \get_field('manifest_label') ?: '',
-            'avatar' => is_array($avatar) ? ($avatar['url'] ?? '') : '',
-            'avatarAlt' => is_array($avatar) ? ($avatar['alt'] ?? '') : '',
             'image' => is_array($image) ? ($image['url'] ?? '') : '',
             'imageAlt' => is_array($image) ? ($image['alt'] ?? '') : '',
         ];
