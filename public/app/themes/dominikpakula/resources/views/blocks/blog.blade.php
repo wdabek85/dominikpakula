9163719cf8e41b03e83f9db90ff0ee94e626adbe
wdabek85 <wdabek85@gmail.com>
2026-07-17 12:15:02 +0200

    Blok blog: naglowki w pierwszej osobie, tematyka meskiego stylu zamiast technologii

--- public/app/themes/dominikpakula/resources/views/blocks/blog.blade.php
+++ public/app/themes/dominikpakula/resources/views/blocks/blog.blade.php
@@ -4,11 +4,11 @@
     {{-- Nagłówek --}}
     <div class="flex flex-col lg:flex-row lg:items-end lg:justify-between gap-7 mb-12">
       <h2 class="font-metro text-4xl leading-[44px] text-black lg:max-w-[726px]">
-        Wiedza, inspiracje i porady – sprawdź nasze <span class="text-[#655098]">Najnowsze</span> artykuły.
+        Styl, inspiracje i porady – zajrzyj do moich <span class="text-[#655098]">najnowszych</span> artykułów.
       </h2>
 
       <p class="font-poppins text-sm leading-4 text-black lg:max-w-[430px]">
-        <span class="font-medium">Na naszym blogu dzielimy się wiedzą, doświadczeniem i aktualnościami ze świata technologii</span>. Sprawdź, co <span class="font-medium">nowego i zainspiruj się</span> naszymi rozwiązaniami.
+        <span class="font-medium">Na blogu dzielę się wiedzą i doświadczeniem ze świata męskiego stylu</span>. Zobacz, co <span class="font-medium">nowego i zainspiruj się</span> do zmian w swoim wizerunku.
       </p>
     </div>
 
